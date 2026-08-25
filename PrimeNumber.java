f4f09ba1ce4b77c27e04093dc2fbe66a91319d4e
Balla Jasmini <jasminiballa4@gmail.com>
2026-08-26 00:13:58 +0530

    Add Week 3 Java control flow problems

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+package control_flow.class_problems;
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+
+            for (int i = 2; i < number; i++) {
+
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(
+            "Is the number " + number + " a Prime number? " + isPrime
+        );
+
+        scanner.close();
+    }
+}
