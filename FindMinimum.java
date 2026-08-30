c762bb09a1811f654bc569a9fffa60aea2a76e20
Balla Jasmini <jasminiballa4@gmail.com>
2026-08-30 13:24:49 +0530

    Add Week 4 Find Minimum

---
+++ FindMinimum.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class FindMinimum {
+
+    public static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum element: " + result);
+
+        scanner.close();
+    }
+}
